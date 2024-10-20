5681235368bfdfce27023b3f95a1fc08b9765aa5
조영우 <choyoungwoo9@naver.com>
2024-10-20 14:21:22 +0900

    feat: 유저 엔티티 추가
    - 유저 인증 서버가 사용하는 유저 엔티티 추가

---
+++ BE/UserService/Server/src/user/entities/user.entity.ts
@@ -0,0 +1,46 @@
+import { Entity, PrimaryGeneratedColumn, Column, Unique } from 'typeorm';
+
+@Entity('user')
+@Unique('UQ_USER_ID', ['userId'])
+@Unique('UQ_NICKNAME', ['nickname'])
+@Unique('UQ_PHONE_NUMBER', ['phoneNumber'])
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'user_id', type: 'varchar', length: 20, nullable: false })
+  userId: string;
+
+  @Column({ type: 'varchar', length: 32, nullable: false })
+  password: string;
+
+  @Column({ type: 'varchar', length: 10, nullable: false })
+  nickname: string;
+
+  @Column({
+    name: 'phone_number',
+    type: 'varchar',
+    length: 11,
+    nullable: false,
+  })
+  phoneNumber: string;
+
+  static of({
+    userId,
+    password,
+    nickname,
+    phoneNumber,
+  }: {
+    userId: string;
+    password: string;
+    nickname: string;
+    phoneNumber: string;
+  }): User {
+    const user = new User();
+    user.userId = userId;
+    user.password = password;
+    user.nickname = nickname;
+    user.phoneNumber = phoneNumber;
+    return user;
+  }
+}
