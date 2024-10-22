63f0acfcab19ed9fd0ffe438021f1f32c668948f
조영우 <choyoungwoo9@naver.com>
2024-10-22 18:54:19 +0900

    chore: 테스트용 레디스 연결로직 삭제

--- BE/UserService/Server/src/app.controller.ts
+++ BE/UserService/Server/src/app.controller.ts
@@ -1,45 +1,12 @@
-import { Controller, Get, OnModuleInit } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import Redis from 'ioredis';
 
 @Controller()
-export class AppController implements OnModuleInit {
-  private redisClient: Redis;
+export class AppController{
   constructor(private readonly appService: AppService) {}
-  onModuleInit() {
-    this.redisClient = new Redis({
-      host: process.env.REDIS_HOST,
-      port: parseInt(process.env.REDIS_PORT),
-    });
-
-    this.redisClient.on('connect', () => {
-      console.log('Connected to Redis');
-    });
-
-    this.redisClient.on('error', (err) => {
-      console.error('Redis connection error:', err);
-    });
-  }
-
-  async onModuleDestroy() {
-    await this.redisClient.quit();
-  }
 
   @Get()
   async getHello() {
-    // await this.testRedisConnection();
     return this.appService.getHello();
-  }
-
-  async testRedisConnection() {
-    try {
-      await this.redisClient.set('test-key', 'test-value');
-      const value = await this.redisClient.get('test-key');
-      console.log('Redis value:', value);
-      return value;
-    } catch (error) {
-      console.error('Error accessing Redis:', error);
-      throw error;
-    }
   }
 }
