1a2aac9d3728a0704718b6392e4bf1c7c0d441f3
이로운 <lru040409@gmail.com>
2024-10-23 15:34:28 +0900

    feat: 알바 공고 작성 페이지 커스텀 캘린더 구현
    - 구현 방법을 모색한다.
        - 라이브러리 사용하지 않고 커스텀 캘린더를 구현하기로 결정
        - 단 날짜 계산을 도와주는 date-fns 라이브러리를 사용한다.
    - 캘린더는 오늘이 있는 주부터 한달 후가 있는 주까지 보인다.
    - 오늘 이전 날짜나 오늘을 기준으로 한 달 이상의 날짜는 비활성화된다.
    - 원하는 여러 날짜를 선택할 수 있다.
    - 캘린더를 드래그하여 날짜를 선택할 수 있다.
    - 각 날짜에 hover 효과와 selected 효과를 준다.
    - 개발 일지를 작성한다.

--- FE/src/components/CreateJobPage/CustomCalendar.tsx
+++ FE/src/components/CreateJobPage/CustomCalendar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import {
   addMonths,
   subDays,
@@ -8,9 +8,11 @@ import {
   format,
   isSameDay,
   compareAsc,
+  isSunday,
 } from "date-fns";
 import { createStitches } from "@stitches/react";
 import { DAYS } from "../../constants/constants.ts";
+import { MainColor } from "../../styles/global.ts";
 
 const { styled } = createStitches();
 
@@ -45,12 +47,33 @@ const Calendar = styled("div", {
     color: "#9A9A9A",
   },
   "& .date": {
+    borderRadius: "100px",
     padding: "12px",
     fontSize: "18px",
     border: "none",
     background: "none",
+    transitionProperty: "background border-radius",
+    transitionDuration: "0.2s",
     "&.selectable": {
       cursor: "pointer",
+      "&.sunday": {
+        color: "#BD2A2E",
+      },
+      "&:hover": {
+        background: "#DCE2FF",
+      },
+      "&.selected": {
+        background: MainColor,
+        color: "white",
+        "&.rightSelected": {
+          borderTopRightRadius: "0",
+          borderBottomRightRadius: "0",
+        },
+        "&.leftSelected": {
+          borderTopLeftRadius: "0",
+          borderBottomLeftRadius: "0",
+        },
+      },
     },
     "&:not(.selectable)": {
       color: "#6E6E6E",
@@ -60,17 +83,52 @@ const Calendar = styled("div", {
 });
 
 const CustomCalendar = () => {
-  const today = new Date();
-  const selectableStart = today; // 선택 가능한 시작 날짜 (오늘)
-  const selectableEnd = subDays(addMonths(today, 1), 1); // 선택 가능한 끝 날짜 (한 달 후)
+  const today = useMemo(() => new Date().setHours(0, 0, 0, 0), []);
+  const selectableStart = useMemo(() => today, [today]); // 선택 가능한 시작 날짜 (오늘)
+  const selectableEnd = useMemo(() => subDays(addMonths(today, 1), 1), [today]); // 선택 가능한 끝 날짜 (한 달 후)
+  const visibleStart = useMemo(
+    () => startOfWeek(selectableStart),
+    [selectableStart]
+  ); // 볼 수 있는 시작 날짜 (오늘이 있는 주의 첫 날)
+  const visibleEnd = useMemo(() => endOfWeek(selectableEnd), [selectableEnd]); // 볼 수 있는 끝 날짜 (한 달 후가 있는 주의 마지막 날)
+  const visibleDays = useMemo(() => {
+    const days = eachDayOfInterval({
+      start: visibleStart,
+      end: visibleEnd,
+    }).map((day) => ({
+      day,
+      isSelectable:
+        compareAsc(selectableStart, day) <= 0 &&
+        compareAsc(day, selectableEnd) <= 0,
+      isSunday: isSunday(day),
+      isToday: isSameDay(today, day),
+    }));
+    return days;
+  }, [visibleStart, visibleEnd, selectableStart, selectableEnd, today]);
 
-  const visibleStart = startOfWeek(selectableStart); // 볼 수 있는 시작 날짜 (오늘이 있는 주의 첫 날)
-  const visibleEnd = endOfWeek(selectableEnd); // 볼 수 있는 끝 날짜 (한 달 후가 있는 주의 마지막 날)
+  const [isSelecteds, setIsSelecteds] = useState<Boolean[]>(
+    new Array(visibleDays.length).fill(false)
+  );
 
-  const visibleDays = eachDayOfInterval({
-    start: visibleStart,
-    end: visibleEnd,
-  });
+  const onDateClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const index = parseInt(e.currentTarget.getAttribute("data-index") || "");
+    if (isNaN(index)) return;
+    setIsSelecteds((prev) =>
+      prev.map((selected, i) => (i === index ? !selected : selected))
+    );
+  };
+
+  const [isDragging, setIsDragging] = useState(false);
+
+  const onMouseMove = (e: React.MouseEvent<HTMLElement>) => {
+    if (!isDragging) return;
+    const element = document.elementFromPoint(e.clientX, e.clientY);
+    const index = parseInt(element?.getAttribute("data-index") || "");
+    if (isNaN(index)) return;
+    setIsSelecteds((prev) =>
+      prev.map((selected, i) => (i === index ? true : selected))
+    );
+  };
 
   return (
     <Container>
@@ -78,21 +136,36 @@ const CustomCalendar = () => {
         {format(selectableStart, "yyyy.MM.dd")}~
         {format(selectableEnd, "yyyy.MM.dd")}
       </div>
-      <Calendar>
+      <Calendar
+        onMouseDown={() => {
+          setIsDragging(true);
+        }}
+        onMouseUp={() => {
+          setIsDragging(false);
+        }}
+        onMouseMove={onMouseMove}
+      >
         {DAYS.map((day) => (
           <div className="weekDay">{day}</div>
         ))}
         {visibleDays &&
-          visibleDays.map((day) => {
-            const date = isSameDay(today, day) ? "오늘" : format(day, "d");
-            // selectabeStart 이상, selectableEnd 이하면 selected 추가
-            // TODO : 오늘 날짜에 selectable이 추가되지 않음
-            const selectable =
-              compareAsc(selectableStart, day) <= 0 &&
-              compareAsc(day, selectableEnd) <= 0;
+          visibleDays.map((dayInfo, index) => {
+            const classNames = ["date"];
+            if (dayInfo.isSelectable) classNames.push("selectable");
+            if (isSelecteds[index]) classNames.push("selected");
+            if (index !== 0 && isSelecteds[index - 1])
+              classNames.push("leftSelected");
+            if (index !== visibleDays.length && isSelecteds[index + 1])
+              classNames.push("rightSelected");
             return (
-              <button className={`date ${selectable ? "selectable" : ""}`}>
-                {date}
+              <button
+                className={classNames.join(" ")}
+                disabled={!dayInfo.isSelectable}
+                key={format(dayInfo.day, "MM.dd")}
+                data-index={index}
+                onClick={onDateClick}
+              >
+                {dayInfo.isToday ? "오늘" : format(dayInfo.day, "d")}
               </button>
             );
           })}
