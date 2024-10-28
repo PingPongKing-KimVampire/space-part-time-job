854721a0a37d9c79639f86a503939ed4829e32ec
조영우 <choyoungwoo9@naver.com>
2024-10-28 14:16:50 +0900

    test: 로그인 E2E 테스트 추가

---
+++ BE/UserService/Server/test/user/login/user.login.e2e-spec.ts
@@ -0,0 +1,77 @@
+import * as request from 'supertest';
+import { getUserDto1, signup } from '../user.utils';
+import { app, clearDatabase, setupTestingApp } from '../user.test-setup.util';
+
+describe('아이디/비밀번호 로그인 (e2e)', () => {
+  beforeAll(async () => {
+    await setupTestingApp();
+  });
+
+  beforeEach(async () => {
+    await clearDatabase();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('로그인 성공', async () => {
+    const userDto = getUserDto1();
+    await signup(app, userDto);
+
+    const loginRequest = {
+      id: userDto.id,
+      password: userDto.password,
+    };
+
+    await request(app.getHttpServer())
+      .post('/api/users/login')
+      .set('Content-Type', 'application/json; charset=utf-8')
+      .send(loginRequest)
+      .expect(201)
+      .expect((response) => {
+        expect(response.body).toHaveProperty('id');
+      });
+  });
+
+  it('로그인 실패 (잘못된 비밀번호)', async () => {
+    const userDto = getUserDto1();
+    await signup(app, userDto);
+
+    const loginRequest = {
+      id: userDto.id,
+      password: 'wrongPassword',
+    };
+
+    await request(app.getHttpServer())
+      .post('/api/users/login')
+      .set('Content-Type', 'application/json; charset=utf-8')
+      .send(loginRequest)
+      .expect(401)
+      .expect((response) => {
+        expect(response.body).toHaveProperty(
+          'error',
+          '아이디/패스워드 로그인 실패',
+        );
+      });
+  });
+
+  it('로그인 실패 (존재하지 않는 아이디)', async () => {
+    const loginRequest = {
+      id: 'invalidId',
+      password: 'invalidPassword',
+    };
+
+    await request(app.getHttpServer())
+      .post('/api/users/login')
+      .set('Content-Type', 'application/json; charset=utf-8')
+      .send(loginRequest)
+      .expect(401)
+      .expect((response) => {
+        expect(response.body).toHaveProperty(
+          'error',
+          '아이디/패스워드 로그인 실패',
+        );
+      });
+  });
+});
