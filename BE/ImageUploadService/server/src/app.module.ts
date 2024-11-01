5f941d84efa1467e19b22d6d7ee601c6016aac53
조영우 <choyoungwoo9@naver.com>
2024-11-01 21:45:03 +0900

    chore: 이미지 업로드 서버 - 몽고DB 연결

--- BE/ImageUploadService/server/src/app.module.ts
+++ BE/ImageUploadService/server/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ImageUploadModule } from './image-upload/image-upload.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: `mongodb://${configService.get('MONGO_HOST')}:${configService.get('MONGO_PORT')}/${configService.get('MONGO_DB')}`,
+      }),
+      inject: [ConfigService],
+    }),
+    ImageUploadModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
