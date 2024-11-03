373738bef3248183a8d01871821bd4ffbdd10414
조영우 <choyoungwoo9@naver.com>
2024-11-03 13:03:13 +0900

    feat: 이미지 업로드 서버에서 grpc를 사용한 인증 서비스 구현

--- BE/ImageUploadService/server/src/image-upload/image-upload.module.ts
+++ BE/ImageUploadService/server/src/image-upload/image-upload.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ImageUploadService } from './image-upload.service';
 import { ImageUploadController } from './image-upload.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [ImageUploadController],
   providers: [ImageUploadService],
+  imports: [ConfigModule]
 })
 export class ImageUploadModule {}
