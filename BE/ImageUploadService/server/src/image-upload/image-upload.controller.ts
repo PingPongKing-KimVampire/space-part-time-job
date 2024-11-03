84b5f43e40a07bb0eb982f1f50583b53c6e011b7
조영우 <choyoungwoo9@naver.com>
2024-11-03 16:30:39 +0900

    feat: 이미지 업로드 서버에 이미지 전달하는 기능(원격서버 X)
    - nginx에 이미지 업로드 서버 반영 및 body 크기 제한 변경
    - 이미지 업로드 서버
      - multer 설치
      - 이미지 받아서 크기 확인하는 기능 구현

--- BE/ImageUploadService/server/src/image-upload/image-upload.controller.ts
+++ BE/ImageUploadService/server/src/image-upload/image-upload.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ImageUploadService } from './image-upload.service';
+import { FilesInterceptor } from '@nestjs/platform-express';
 
-@Controller('image-upload')
-export class ImageUploadController {}
+@Controller('/api/image-upload')
+export class ImageUploadController {
+  constructor(private readonly imageUploadService: ImageUploadService) {}
+
+  @Post('')
+  @UseInterceptors(FilesInterceptor('imageFiles'))
+  uploadFiles(@UploadedFiles() files: Array<Express.Multer.File>) {
+    if (!files) throw new HttpException('파일이 없습니다.', 400);
+    if (files.length > 10)
+      throw new HttpException(`최대 파일개수 초과`, HttpStatus.BAD_REQUEST);
+
+    const maxSize = 1000 * 1000 * 10;
+    for (const file of files) {
+      if (file.size > maxSize) {
+        throw new HttpException(
+          `파일 크기 초과 (최대 10MB)`,
+          HttpStatus.PAYLOAD_TOO_LARGE,
+        );
+      }
+    }
+  }
+}
