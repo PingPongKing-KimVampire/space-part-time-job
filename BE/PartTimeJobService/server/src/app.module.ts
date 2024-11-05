43f1d1fd715810477f4b3e9ef310652642dd6b5a
조영우 <choyoungwoo9@naver.com>
2024-11-05 21:11:21 +0900

    chore: 알바 공고 서버와 알바 공고 DB를 연결

--- BE/PartTimeJobService/server/src/app.module.ts
+++ BE/PartTimeJobService/server/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PartTimeJobModule } from './part-time-job/part-time-job.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: `mongodb://${configService.get('MONGO_HOST')}:${configService.get('MONGO_PORT')}/${configService.get('MONGO_DB')}`,
+      }),
+      inject: [ConfigService],
+    }),
+    PartTimeJobModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
