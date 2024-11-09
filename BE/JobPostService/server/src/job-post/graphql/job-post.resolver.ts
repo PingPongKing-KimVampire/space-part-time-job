dcb3b9cf83ff945b08768c01f09641b39e6c1f3b
조영우 <choyoungwoo9@naver.com>
2024-11-09 11:46:30 +0900

    feat: 인증된 사용자만 알바 공고 업로드가 가능하도록 구현
    - 쿠키파서 설치

--- BE/JobPostService/server/src/job-post/graphql/job-post.resolver.ts
+++ BE/JobPostService/server/src/job-post/graphql/job-post.resolver.ts
@@ -1,13 +1,33 @@
-import { Args, ID, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Context, ID, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { JobPostRepository } from '../mongoose/job-post.repository';
 import { CreateJobPostInput } from './dto/job-post.input.dto';
+import { UserService } from '../user/user.service';
+import { Request } from 'express';
+import { HttpException } from '@nestjs/common';
 
 @Resolver('JobPost')
 export class JobPostResolver {
-  constructor(private readonly jobPostService: JobPostRepository) {}
+  constructor(
+    private readonly jobPostService: JobPostRepository,
+    private readonly userService: UserService,
+  ) {}
   @Mutation()
-  async createJobPost(@Args('input') input: CreateJobPostInput) {
-    const userId = 'tempId';
+  async createJobPost(
+    @Args('input') input: CreateJobPostInput,
+    @Context('req') req: Request,
+  ) {
+    const accessToken = req.cookies.access_token;
+    let userId;
+    if (!accessToken) throw new HttpException('토큰 없음', 401);
+    try {
+      userId = await this.userService.authenticateUser(
+        req.cookies.access_token,
+      );
+    } catch (e) {
+      if (e.message === '유저 인증 실패')
+        throw new HttpException('유저 인증 실패', 401);
+      throw e;
+    }
     return this.jobPostService.createJobPost(input, userId);
   }
 
