b826af7aa9ea311ec077a79903e6cbc9297ef5bc
조영우 <choyoungwoo9@naver.com>
2024-12-10 21:50:26 +0900

    test: 아르바이트 공고 게시 테스트의 input을 grpc형식에 맞게 수정

--- BE/JobPostService/server/src/job-post/grpc/dto/create-job-post/job-post.input.dto.spec.ts
+++ BE/JobPostService/server/src/job-post/grpc/dto/create-job-post/job-post.input.dto.spec.ts
@@ -1,5 +1,4 @@
-import "reflect-metadata"; //없으면 테스트시 Reflect.getMetadata is not a function 오류가 생김
-
+import 'reflect-metadata'; // Reflect.getMetadata 관련 오류 방지
 import { validate } from 'class-validator';
 import { CreateJobPostInput } from './job-post.input.dto';
 import {
@@ -10,34 +9,59 @@ import {
 } from 'src/job-post/mongoose/job-post.enum';
 import { plainToClass } from 'class-transformer';
 import { addDays, addMonths, format } from 'date-fns';
+import {
+  DayOfWeekMapping,
+  JobCategoryMapping,
+  SalaryTypeMapping,
+  WorkPeriodTypeMapping,
+  WorkTimeTypeMapping,
+} from '../../job-post.enum-mapping';
+
+function reverseMapping(
+  mapping: Record<number, string>,
+): Record<number, string> {
+  return Object.entries(mapping).reduce((reversed, [key, value]) => {
+    reversed[value] = key;
+    return reversed;
+  }, {});
+}
+
+const ReverseJobCategoryMapping = reverseMapping(JobCategoryMapping);
+const ReverseDayOfWeekMapping = reverseMapping(DayOfWeekMapping);
+const ReverseWorkPeriodTypeMapping = reverseMapping(WorkPeriodTypeMapping);
+const ReverseWorkTimeTypeMapping = reverseMapping(WorkTimeTypeMapping);
+const ReverseSalaryTypeMapping = reverseMapping(SalaryTypeMapping);
 
 describe('CreateJobPostInput 유효성 검사', () => {
   const getValidInput = () => {
     const today = new Date();
-    const input = new CreateJobPostInput();
+    const input: any = new CreateJobPostInput();
     input.title = '유효한 제목';
-    input.jobDescription = [JobCategory.CLEANING];
+    input.userId = 'userId';
+    input.jobDescription = [ReverseJobCategoryMapping[JobCategory.BAKING]];
     input.workPeriod = {
-      type: WorkPeriodType.SHORT_TERM,
+      type: ReverseWorkPeriodTypeMapping[WorkPeriodType.SHORT_TERM],
       dates: [format(today, 'yyyy-MM-dd')],
     };
     input.workTime = {
-      type: WorkTimeType.FIXED,
+      type: ReverseWorkTimeTypeMapping[WorkTimeType.FIXED],
       startTime: '09:00',
       endTime: '17:00',
     };
     input.salary = {
-      salaryType: SalaryType.HOURLY,
+      salaryType: ReverseSalaryTypeMapping[SalaryType.HOURLY],
       salaryAmount: 10000,
     };
     input.photos = ['http://example.com/photo1.jpg'];
     input.detailedDescription = '15자가 넘는 유효한 상세 설명입니다.';
+    input.addressName = '동호로 97';
     return input;
   };
 
   it('올바른 입력값으로 유효성 검사 통과', async () => {
     const validInput = plainToClass(CreateJobPostInput, getValidInput());
     const errors = await validate(validInput);
+    console.log(errors);
     expect(errors.length).toBe(0);
   });
 
@@ -68,10 +92,10 @@ describe('CreateJobPostInput 유효성 검사', () => {
   it('하는 일 목록이 3개를 초과할 때 실패', async () => {
     const input = getValidInput();
     input.jobDescription = [
-      JobCategory.CLEANING,
-      JobCategory.BAKING,
-      JobCategory.BEVERAGE_MAKING,
-      JobCategory.CONVENIENCE_STORE,
+      ReverseJobCategoryMapping[JobCategory.CLEANING],
+      ReverseJobCategoryMapping[JobCategory.BAKING],
+      ReverseJobCategoryMapping[JobCategory.BEVERAGE_MAKING],
+      ReverseJobCategoryMapping[JobCategory.CONVENIENCE_STORE],
     ];
     const transformedInput = plainToClass(CreateJobPostInput, input);
     const errors = await validate(transformedInput);
@@ -98,7 +122,10 @@ describe('CreateJobPostInput 유효성 검사', () => {
 
   it('LONG_TERM 근무 유형일 때 요일이 없으면 실패', async () => {
     const input = getValidInput();
-    input.workPeriod = { type: WorkPeriodType.LONG_TERM, days: [] };
+    input.workPeriod = {
+      type: ReverseWorkPeriodTypeMapping[WorkPeriodType.LONG_TERM],
+      days: [],
+    };
     const transformedInput = plainToClass(CreateJobPostInput, input);
     const errors = await validate(transformedInput);
     expect(errors.length).toBeGreaterThan(0);
@@ -107,7 +134,7 @@ describe('CreateJobPostInput 유효성 검사', () => {
   it('LONG_TERM 근무 유형일 때 요일 형식이 올바르지 않을 때 실패', async () => {
     const input = getValidInput();
     input.workPeriod = {
-      type: WorkPeriodType.LONG_TERM,
+      type: ReverseWorkPeriodTypeMapping[WorkPeriodType.LONG_TERM],
       days: ['INVALID_DAY'],
     };
     const transformedInput = plainToClass(CreateJobPostInput, input);
@@ -117,7 +144,10 @@ describe('CreateJobPostInput 유효성 검사', () => {
 
   it('SHORT_TERM 근무 유형일 때 요일이 있으면 실패', async () => {
     const input = getValidInput();
-    input.workPeriod = { type: WorkPeriodType.SHORT_TERM, days: ['MONDAY'] };
+    input.workPeriod = {
+      type: ReverseWorkPeriodTypeMapping[WorkPeriodType.SHORT_TERM],
+      days: [ReverseDayOfWeekMapping['MONDAY']],
+    };
     const transformedInput = plainToClass(CreateJobPostInput, input);
     const errors = await validate(transformedInput);
     expect(errors.length).toBeGreaterThan(0);
@@ -125,7 +155,9 @@ describe('CreateJobPostInput 유효성 검사', () => {
 
   it('FIXED 근무 시간이 없을 때 실패', async () => {
     const input = getValidInput();
-    input.workTime = { type: WorkTimeType.FIXED };
+    input.workTime = {
+      type: ReverseWorkTimeTypeMapping[WorkTimeType.FIXED],
+    };
     const transformedInput = plainToClass(CreateJobPostInput, input);
     const errors = await validate(transformedInput);
     expect(errors.length).toBeGreaterThan(0);
@@ -141,7 +173,9 @@ describe('CreateJobPostInput 유효성 검사', () => {
 
   it('유효하지 않은 WorkPeriodType일 때 실패', async () => {
     const input = getValidInput();
-    input.workPeriod = { type: 'INVALID_TYPE' as WorkPeriodType };
+    input.workPeriod = {
+      type: 'INVALID_TYPE',
+    };
     const transformedInput = plainToClass(CreateJobPostInput, input);
     const errors = await validate(transformedInput);
     expect(errors.length).toBeGreaterThan(0);
@@ -150,7 +184,7 @@ describe('CreateJobPostInput 유효성 검사', () => {
   it('유효하지 않은 SalaryType일 때 실패', async () => {
     const input = getValidInput();
     input.salary = {
-      salaryType: 'INVALID_TYPE' as SalaryType,
+      salaryType: 'INVALID_TYPE',
       salaryAmount: 10000,
     };
     const transformedInput = plainToClass(CreateJobPostInput, input);
