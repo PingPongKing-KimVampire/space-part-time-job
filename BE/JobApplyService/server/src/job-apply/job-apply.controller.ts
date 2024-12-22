3f8e18444d01888fefd610a5328bba89c391363f
조영우 <choyoungwoo9@naver.com>
2024-12-22 16:57:26 +0900

    feat: 아르바이트 지원 시 필드 유무, 길이에 대해 유효성 검사하도록 구현

--- BE/JobApplyService/server/src/job-apply/job-apply.controller.ts
+++ BE/JobApplyService/server/src/job-apply/job-apply.controller.ts
@@ -1,11 +1,13 @@
 import { Controller } from '@nestjs/common';
-import { GrpcMethod } from '@nestjs/microservices';
+import { GrpcMethod, RpcException } from '@nestjs/microservices';
 import { ApplyToJobPostRequest } from './grpc/dto/apply-to-job-post/request.dto';
 import {
   ApplyToJobPostResponse,
   ApplyToJobPostResponseStatus,
 } from './grpc/dto/apply-to-job-post/response.dto';
 import { JobApplyService } from './job-apply.service';
+import { plainToInstance } from 'class-transformer';
+import { validate, ValidationError } from 'class-validator';
 
 @Controller()
 export class JobApplyController {
@@ -14,6 +16,8 @@ export class JobApplyController {
   async applyToJobPost(
     request: ApplyToJobPostRequest,
   ): Promise<ApplyToJobPostResponse> {
+    request = plainToInstance(ApplyToJobPostRequest, request);
+    await this.validateFormat(request);
     const jobApplication = await this.jobApplyService.applyToJobPost({
       userId: request.userId,
       jobPostId: request.jobPostId,
@@ -29,5 +33,30 @@ export class JobApplyController {
         status: ApplyToJobPostResponseStatus[jobApplication.status],
       },
     };
+  }
+
+  private async validateFormat<T extends object>(input: T): Promise<void> {
+    const errors = await validate(input);
+    if (errors.length > 0) {
+      const errorMessage = this.getErrorMessage(errors);
+      throw new RpcException(errorMessage);
+    }
+  }
+
+  private getErrorMessage(errors: ValidationError[]) {
+    const errorMessage = errors
+      .flatMap((error) =>
+        Object.values(error.constraints || [])
+          .map((msg) => `${error.property}: ${msg}`)
+          .concat(
+            error.children?.flatMap((child) =>
+              Object.values(child.constraints || []).map(
+                (msg) => `${child.property}: ${msg}`,
+              ),
+            ) || [],
+          ),
+      )
+      .join('; ');
+    return errorMessage;
   }
 }
