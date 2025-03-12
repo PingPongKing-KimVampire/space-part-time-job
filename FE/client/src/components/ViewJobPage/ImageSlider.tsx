9ebf648fdcc9ea1f2456efd37f39457cb8322410
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2025-03-12 10:22:07 +0900

    refactor: 이미지 슬라이더 코드 개선

--- FE/client/src/components/ViewJobPage/ImageSlider.tsx
+++ FE/client/src/components/ViewJobPage/ImageSlider.tsx
@@ -10,57 +10,51 @@ type ImageSliderProps = {
 
 const ImageSlider: React.FC<ImageSliderProps> = ({ imageUrls, loading }) => {
   const imageListRef = useRef<HTMLDivElement | null>(null);
-
-  const [currentIndex, setCurrentIndex] = useState(0); // 현재 보여지는 이미지의 인덱스
-  const [containerWidth, setContainerWidth] = useState(0); // 한 번 슬라이딩 시 움직여야 하는 거리
-
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const setContainerRef = useCallback((node: HTMLDivElement | null) => {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [containerWidth, setContainerWidth] = useState(0);
+
+  const setContainerhRef = useCallback((node: HTMLDivElement | null) => {
     if (!node) return;
     setContainerWidth(node.clientWidth);
     containerRef.current = node;
   }, []);
 
   useEffect(() => {
-    let resizeTimeout: ReturnType<typeof setTimeout> | null = null;
+    const updateContainerWidth = () => {
+      if (!containerRef.current) return;
+      setContainerWidth(containerRef.current.clientWidth);
+    };
 
-    const handleResize = () => {
-      if (!imageListRef.current || !containerRef.current) return;
-      // resize 동안 transition 제거, transfrom 직접 업데이트
-      imageListRef.current.style.transition = "none";
-      imageListRef.current.style.transform = `translateX(-${
-        containerRef.current.clientWidth * currentIndex
-      }px)`;
-      // resize 종료 후 일정 시간 뒤 transition 복원 및 containerWidth 업데이트 (debounce)
-      if (resizeTimeout) clearTimeout(resizeTimeout);
-      resizeTimeout = setTimeout(() => {
-        if (!imageListRef.current || !containerRef.current) return;
-        setContainerWidth(containerRef.current.clientWidth);
-        imageListRef.current.style.transition = "transform 0.5s";
-      }, 200);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-      if (resizeTimeout) clearTimeout(resizeTimeout);
-    };
+    window.addEventListener("resize", updateContainerWidth);
+    return () => window.removeEventListener("resize", updateContainerWidth);
+  }, []);
+
+  useEffect(() => {
+    if (!imageListRef.current) return;
+    imageListRef.current.style.transition = "transform 0.5s";
+    imageListRef.current.style.transform = `translateX(-${
+      containerWidth * currentIndex
+    }px)`;
   }, [currentIndex]);
 
-  const slidingTo = (index: number) => {
-    if (index < 0 || imageUrls.length <= index) return;
+  useEffect(() => {
     if (!imageListRef.current) return;
-    const dist = currentIndex - index; // 이동해야 하는 거리 (방향 포함)
-    const currentPos = -(containerWidth * currentIndex);
-    const newPos = currentPos + dist * containerWidth;
+    imageListRef.current.style.transition = "none";
+    imageListRef.current.style.transform = `translateX(-${
+      containerWidth * currentIndex
+    }px)`;
+  }, [containerWidth]);
+
+  const slidingTo = (index: number) => {
+    if (index < 0 || index >= imageUrls.length) return;
     setCurrentIndex(index);
-    imageListRef.current.style.transform = `translateX(${newPos}px)`;
   };
 
-  if (loading) {
-    return <ImageSliderContainer className="loading" />;
-  }
+  if (loading) return <ImageSliderContainer className="loading" />;
   return (
-    <ImageSliderContainer ref={setContainerRef}>
+    <ImageSliderContainer ref={setContainerhRef}>
       <div className="imageList" ref={imageListRef}>
         {imageUrls.map((url) => (
           <div className="imageBox" key={url}>
@@ -70,10 +64,10 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ imageUrls, loading }) => {
       </div>
       <div className="indicatorContainer">
         <div className="indicator">
-          {Array.from({ length: imageUrls.length }).map((_, index) => (
+          {imageUrls.map((url, index) => (
             <button
               className={`item ${index === currentIndex ? "active" : ""}`}
-              key={imageUrls[index]}
+              key={url}
               onClick={() => slidingTo(index)}
             />
           ))}
@@ -85,7 +79,7 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ imageUrls, loading }) => {
       />
       <RightArrowIcon
         className={`right ${
-          imageUrls.length - 1 <= currentIndex ? "inactive" : ""
+          currentIndex >= imageUrls.length - 1 ? "inactive" : ""
         }`}
         onClick={() => slidingTo(currentIndex + 1)}
       />
